abaa75493a41b5d22c12d3ac2297925297602fdc
Yash Pal <ypal23808@gmail.com>
2023-10-14 20:29:25 +0530

    args in java

---
+++ practices/Cmd.java
@@ -0,0 +1,9 @@
+// command line arguments
+public class Cmd {
+    public static void main(String args[]){
+        for(int i=0; i<args.length;i++){
+        System.out.println(args[i]);
+        }
+    }
+    
+}
