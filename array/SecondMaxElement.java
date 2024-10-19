28fd60a2bb645387f2583ca12bf2ac1bddbfe39d
Yash Pal <ypal23808@gmail.com>
2024-10-19 20:46:12 +0530

    first repeating element in an array

--- array/SecondMaxElement.java
+++ array/SecondMaxElement.java
@@ -19,11 +19,27 @@ public class SecondMaxElement {
         }
         System.out.println("Second maximum element is " + second);
     }
+    
+    static int firstRepeatElement(int[] arr){
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] == arr[j]){
+                    return arr[i]; // present
+                }
+            }
+        }
+        return -1; // not present 
+
+    }
     public static void main(String[] args) {
         
         int[] arr = {2,6,4,3,8,5,1};
+        int[] arr1 = {3,1,2,3,4,5,2};
 
         // second max element
-        secondMaxElement(arr);
+        // secondMaxElement(arr);
+        int res = firstRepeatElement(arr1);
+        System.out.println(res);
     }
 }
